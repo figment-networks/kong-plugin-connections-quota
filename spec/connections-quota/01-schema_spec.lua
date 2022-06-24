559cd9f443e3e802431242a6e7344e6ce941e869
Matt Kornatz <mkornatz@figment.io>
2022-06-24 17:24:19 -0400

    Adds new config options to spec

--- spec/connections-quota/01-schema_spec.lua
+++ spec/connections-quota/01-schema_spec.lua
@@ -3,6 +3,8 @@ local helpers = require "spec.helpers"
 local PLUGIN_NAME = "connections-quota"
 local redis_host  = helpers.redis_host
 local redis_port  = 6379
+local redis_password = 'abc123$'
+local redis_ssl = true
 local schema_def = require("kong.plugins."..PLUGIN_NAME..".schema")
 local v = helpers.validate_plugin_config_schema
 
@@ -21,6 +23,8 @@ describe("Plugin: " .. PLUGIN_NAME .. " (schema), ", function()
       policy = "redis",
       redis_host = redis_host,
       redis_port = redis_port,
+      redis_password = redis_password,
+      redis_ssl = redis_ssl,
       services_limits = {
         ["avalanche--fuji--rpc.datahub.figment.io"] = {
           ["second"] = 10
