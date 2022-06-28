2053274078764ac5a4c81cbc2187c66799816110
Matt Kornatz <mkornatz@users.noreply.github.com>
2022-06-28 12:15:56 -0400

    Adds support for redis ssl and username (#3)
    * Adds support for redis ssl and username
    
    * Adds new config options to spec
    
    * ssl settings are not required
    
    * Separate spec

--- spec/connections-quota/01-schema_spec.lua
+++ spec/connections-quota/01-schema_spec.lua
@@ -3,6 +3,9 @@ local helpers = require "spec.helpers"
 local PLUGIN_NAME = "connections-quota"
 local redis_host  = helpers.redis_host
 local redis_port  = 6379
+local redis_password = 'abc123$'
+local redis_ssl = true
+local redis_ssl_verify = true
 local schema_def = require("kong.plugins."..PLUGIN_NAME..".schema")
 local v = helpers.validate_plugin_config_schema
 
@@ -26,6 +29,18 @@ describe("Plugin: " .. PLUGIN_NAME .. " (schema), ", function()
           ["second"] = 10
         }
       }
+    }
+    assert(v(minimal_conf, schema_def))
+  end)
+
+  it("redis password and ssl config", function()
+    local minimal_conf = {
+      policy = "redis",
+      redis_host = redis_host,
+      redis_port = redis_port,
+      redis_password = redis_password,
+      redis_ssl = redis_ssl,
+      redis_ssl_verify = redis_ssl_verify
     }
     assert(v(minimal_conf, schema_def))
   end)
