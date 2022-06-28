65d104b617295b02df47fb93d4392dd7c233dd10
Matt Kornatz <mkornatz@figment.io>
2022-06-28 11:36:54 -0400

    Separate spec

--- spec/connections-quota/01-schema_spec.lua
+++ spec/connections-quota/01-schema_spec.lua
@@ -5,6 +5,7 @@ local redis_host  = helpers.redis_host
 local redis_port  = 6379
 local redis_password = 'abc123$'
 local redis_ssl = true
+local redis_ssl_verify = true
 local schema_def = require("kong.plugins."..PLUGIN_NAME..".schema")
 local v = helpers.validate_plugin_config_schema
 
@@ -23,13 +24,23 @@ describe("Plugin: " .. PLUGIN_NAME .. " (schema), ", function()
       policy = "redis",
       redis_host = redis_host,
       redis_port = redis_port,
-      redis_password = redis_password,
-      redis_ssl = redis_ssl,
       services_limits = {
         ["avalanche--fuji--rpc.datahub.figment.io"] = {
           ["second"] = 10
         }
       }
+    }
+    assert(v(minimal_conf, schema_def))
+  end)
+
+  it("redis password and ssl config", function()
+    local minimal_conf = {
+      policy = "redis",
+      redis_host = redis_host,
+      redis_port = redis_port,
+      redis_password = redis_password,
+      redis_ssl = redis_ssl,
+      redis_ssl_verify = redis_ssl_verify
     }
     assert(v(minimal_conf, schema_def))
   end)
